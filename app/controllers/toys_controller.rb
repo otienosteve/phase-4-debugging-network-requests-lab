c7e4d43edfdd55799f3a5abc259e0485d84ea7a5
otienosteve <steveotieno701@gmail.com>
2022-09-08 21:42:37 +0300

    complete

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,25 +7,37 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    # byebug 
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
+    if toy
     toy.update(toy_params)
+    render json: toy, status: :accepted 
+    else
+      render json: {error:"error processing request"}
+    end
+    
+    
   end
 
   def destroy
     toy = Toy.find_by(id: params[:id])
+    if toy
     toy.destroy
     head :no_content
+    else
+    render json: {error:"toy not found"}
+    end
   end
 
   private
   
   def toy_params
-    params.permit(:name, :image, :likes)
+    params.permit(:name,:image,:likes)
   end
 
 end
